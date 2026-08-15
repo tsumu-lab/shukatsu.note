093950a9714e384ba43c33fbcf6fd5fefbe6a869
糸田川大輔 <itotagawadaisuke@itotagawadaisukenoMacBook-Air.local>
2026-08-15 22:23:51 +0900

    DB接続をプーリングに切り替え

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: env("DIRECT_URL"), // ← ここをDATABASE_URLから変更
   },
 });
